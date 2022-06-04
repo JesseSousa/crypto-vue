d6d9e88b383b7fe4427936e649b9f0360793c02d
JesseSousa <rhadamesjesse14rj@gmail.com>
2022-06-04 19:58:58 -0300

    feat(TheNavbar): create template

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,9 @@
+<template>
+  <nav class="bg-emerald-400 w-full py-4">
+    <div class="container mx-auto">
+      <h1 class="text-2xl text-white font-semibold font-mono">Crypto</h1>
+    </div>
+  </nav>
+</template>
+
+<script setup></script>
