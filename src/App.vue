ef19fcd139145121d48aae977642b6478c7f6f13
JesseSousa <rhadamesjesse14rj@gmail.com>
2022-06-04 20:05:15 -0300

    feat(App): render app-container and the-navbar components

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
-<script setup></script>
+<script setup>
+import AppContainer from './components/AppContainer.vue';
+import TheNavbar from './components/TheNavbar.vue';
+</script>
 
-<template></template>
+<template>
+  <div>
+    <TheNavbar />
+    <app-container></app-container>
+  </div>
+</template>
 
 <style></style>
