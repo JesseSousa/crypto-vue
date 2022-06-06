8646249909fd40ec3a87ae1ffa91ddde7bf9b674
JesseSousa <rhadamesjesse14rj@gmail.com>
2022-06-06 11:29:29 -0300

    feat(CoinDashboardPrice.vue): create and style coin dashboard price component

---
+++ src/components/coin-dashboard/CoinDashboardPrice.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex items-start flex-col py-4">
+    <h2 class="text-gray-600 text-lg">Current Price:</h2>
+    <h3 class="font-semibold text-gray-800 text-2xl flex items-center">
+      ${{ props.price }}
+      <span
+        class="text-base bg-gray-200 p-1 text-green-600 rounded flex items-center justify-around ml-2"
+      >
+        <i class="fa-solid fa-caret-up mr-1 text-2xl"></i>
+        {{ props.priceChangePercentage24h }}%
+      </span>
+    </h3>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps(['price', 'priceChangePercentage24h']);
+</script>
