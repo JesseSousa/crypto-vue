46b512bb558bbf3db8ad5b843c13521741d4cf68
JesseSousa <rhadamesjesse14rj@gmail.com>
2022-06-06 11:18:11 -0300

    feat(CoinDashboard.vue): import CoinDashboardTitle and use coinData composable

--- src/components/coin-dashboard/CoinDashboard.vue
+++ src/components/coin-dashboard/CoinDashboard.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="w-full p-4 md:px-8 md:py-2"></div>
+  <div class="w-full p-4 md:px-8 md:py-2">
+    <CoinDashboardTitle
+      :name="coin.name"
+      :image="coin.image"
+      :symbol="coin.symbol"
+    />
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { useCoinData } from '../../composables/coinData';
+import CoinDashboardTitle from './CoinDashboardTitle.vue';
+
+const coin = useCoinData('bitcoin');
+</script>
