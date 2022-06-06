4759f96b15c8e5b0c703ecaea42cec890fc2d46c
JesseSousa <rhadamesjesse14rj@gmail.com>
2022-06-06 12:32:48 -0300

    feat(CoinDashboardStats.vue): create and style component

---
+++ src/components/coin-dashboard/CoinDashboardStatsItem.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div class="py-2">
+      <h1 class="text-gray-600 text-lg font-semibold">{{ props.label }}</h1>
+      <p class="text-lg px-4">{{ props.value }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps(['label', 'value']);
+</script>
