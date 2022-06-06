fc68c47cd6fa314720cb7139766d48b3c843955f
JesseSousa <rhadamesjesse14rj@gmail.com>
2022-06-06 11:33:08 -0300

    feat(CoinDashboard.vue): import CoinDashboardPrice

--- src/components/coin-dashboard/CoinDashboard.vue
+++ src/components/coin-dashboard/CoinDashboard.vue
@@ -5,12 +5,18 @@
       :image="coin.image"
       :symbol="coin.symbol"
     />
+    <CoinDashboardPrice
+      :price="coin.price"
+      :priceChangePercentage24h="coin.priceChangePercentage24h"
+    />
   </div>
 </template>
 
 <script setup>
 import { useCoinData } from '../../composables/coinData';
+
 import CoinDashboardTitle from './CoinDashboardTitle.vue';
+import CoinDashboardPrice from './CoinDashboardPrice.vue';
 
 const coin = useCoinData('bitcoin');
 </script>
