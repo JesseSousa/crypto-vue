6f29fd2947da793ffd020aca78b892af5823c762
JesseSousa <rhadamesjesse14rj@gmail.com>
2022-06-09 19:39:41 -0300

    feat(App.vue): add global 'coinId' ref

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <script setup>
+import { provide, ref } from 'vue';
+
 import AppContainer from './components/AppContainer.vue';
 import TheNavbar from './components/TheNavbar.vue';
 import CoinDashboard from './components/coin-dashboard/CoinDashboard.vue';
+
+const coinId = ref('bitcoin');
+
+provide('coinId', coinId);
 </script>
 
 <template>
