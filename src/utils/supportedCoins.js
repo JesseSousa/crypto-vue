56b771ed6fb4d67b93bc03618b65d3cf1272428e
JesseSousa <rhadamesjesse14rj@gmail.com>
2022-06-09 12:18:34 -0300

    feat(supportedCoins.js): create array of supported coins with symbol and id

---
+++ src/utils/supportedCoins.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    symbol: 'BTC',
+    id: 'bitcoin',
+  },
+  {
+    symbol: 'DACXI',
+    id: 'dacxi',
+  },
+  {
+    symbol: 'ATOM',
+    id: 'cosmos',
+  },
+  {
+    symbol: 'LUNA',
+    id: 'terra-luna-2',
+  },
+  {
+    symbol: 'ETH',
+    id: 'ethereum',
+  },
+];
