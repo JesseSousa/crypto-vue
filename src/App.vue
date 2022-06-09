a0cd2697b2e1cc26ad73095e1fbb3cd286d6d441
JesseSousa <rhadamesjesse14rj@gmail.com>
2022-06-09 13:51:38 -0300

    feat(App.vue): render router view

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <script setup>
 import AppContainer from './components/AppContainer.vue';
 import TheNavbar from './components/TheNavbar.vue';
-import CoinDashboard from './components/coin-dashboard/CoinDashboard.vue';
 </script>
 
 <template>
   <div>
     <TheNavbar />
     <app-container>
-      <CoinDashboard />
+      <router-view></router-view>
     </app-container>
   </div>
 </template>
