17491ce4da3b33e9bdd4396578c3843585a48550
JesseSousa <rhadamesjesse14rj@gmail.com>
2022-06-09 19:42:13 -0300

    feat(AppCoinSelect.vue): create coin select component

---
+++ src/components/AppCoinSelect.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="w-full flex justify-center pt-4">
+    <div>
+      <label class="text-gray-700">Choose a coin: </label>
+      <select v-model="selectedCoin" class="p-2 rounded">
+        <option v-for="coin of coins" :key="coin.id" :value="coin.id">
+          {{ coin.symbol }}
+        </option>
+      </select>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, inject } from 'vue';
+import supportedCoins from '../utils/supportedCoins';
+
+const coinId = inject('coinId');
+
+const coins = ref(supportedCoins);
+const selectedCoin = ref(coinId);
+</script>
