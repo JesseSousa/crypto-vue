6bd67bf13fdbf8c79ba13f85219a647b07e0687b
JesseSousa <rhadamesjesse14rj@gmail.com>
2022-06-09 20:01:58 -0300

    feat(TheNavbar.vue): update title

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,7 +1,11 @@
 <template>
   <nav class="bg-emerald-400 w-full py-4">
     <div class="container mx-auto">
-      <h1 class="text-2xl text-white font-semibold font-mono">Crypto</h1>
+      <h1
+        class="text-2xl text-white font-semibold font-mono text-center md:text-left"
+      >
+        CryptoVue
+      </h1>
     </div>
   </nav>
 </template>
